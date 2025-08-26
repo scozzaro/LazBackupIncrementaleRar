bb14bd4e6a95b92669dbb26841df545a32b47a8a
Vincenzo Scozzaro <scozzaro@gmail.com>
2025-08-26 13:49:45 +0200

    Add files via upload

--- aboutform.pas
+++ aboutform.pas
@@ -5,7 +5,8 @@ unit AboutForm;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  RichMemo, ShellAPI;
 
 type
 
@@ -19,8 +20,12 @@ type
     LblLicenza: TLabel;
     LblCopyright: TLabel;
     LblVersion: TLabel;
+    LblWinRARWarning: TLabel;
     MemoTestoLicenza: TMemo;
+    RichMemo1: TRichMemo;
     procedure BtnClose1Click(Sender: TObject);
+    procedure LblLicenzaClick(Sender: TObject);
+    procedure LblWinRARWarningClick(Sender: TObject);
   private
 
   public
@@ -40,6 +45,29 @@ procedure TfrmAbout.BtnClose1Click(Sender: TObject);
 begin
   close;
 end;
+procedure TfrmAbout.LblLicenzaClick(Sender: TObject);
+begin
+  if ShellExecute(0, PChar('open'),PChar('https://www.mozilla.org/MPL/2.0/'), nil, nil,1) =0 then;
+end;
+
+procedure TfrmAbout.LblWinRARWarningClick(Sender: TObject);
+
+
+ var
+  RTFStream: TFileStream;
+begin
+  // Crea un TFileStream per leggere il file RTF
+
+  RTFStream := TFileStream.Create('licenza.rtf', fmOpenRead or fmShareDenyWrite);
+  try
+    // Carica il contenuto del flusso nel componente RichMemo
+    RichMemo1.LoadRichText(RTFStream);
+  finally
+    // Libera la memoria del TFileStream
+    RTFStream.Free;
+  end;
+end;
+
 
 end.
 
